6a260a8d260db27fbf6ee6c1dc04e16e07d77dbc
simonwinther <simon.winther@hotmail.com>
2018-01-12 14:27:02 +0100

    worked on the view contorller.

--- Gittraining/ViewController.m
+++ Gittraining/ViewController.m
@@ -11,12 +11,12 @@
 @interface ViewController ()
 
 @end
-
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"hej");
 }
 
 
